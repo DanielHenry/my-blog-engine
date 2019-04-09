e52cd23989a7cb099c1aacd04a92ee79c3385eb9
DanielHenry <daniel.henry.39@gmail.com>
2019-04-09 16:18:34 +0700

    Initial ArticleRepository.java

---
+++ src/main/java/com/danielhenry/myblogengine/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.danielhenry.myblogengine.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.danielhenry.myblogengine.entity.Article;;
+
+interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
