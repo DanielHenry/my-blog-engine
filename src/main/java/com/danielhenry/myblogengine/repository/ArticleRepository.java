04fc403b530d42cbe4b1ef0648769d544f5e9f37
DanielHenry <daniel.henry.39@gmail.com>
2019-04-09 16:20:37 +0700

    Remove double semicolon in ArticleRepository.java

--- src/main/java/com/danielhenry/myblogengine/repository/ArticleRepository.java
+++ src/main/java/com/danielhenry/myblogengine/repository/ArticleRepository.java
@@ -2,7 +2,7 @@ package com.danielhenry.myblogengine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.danielhenry.myblogengine.entity.Article;;
+import com.danielhenry.myblogengine.entity.Article;
 
 interface ArticleRepository extends JpaRepository<Article, Long> {
 
