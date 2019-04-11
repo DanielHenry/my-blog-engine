301a3d4af5720627c503fe69d76ea1bac73d3325
DanielHenry <daniel.henry.39@gmail.com>
2019-04-11 12:37:30 +0700

    add serialVersionUID in ArticleNotFoundException

--- src/main/java/com/danielhenry/myblogengine/exception/ArticleNotFoundException.java
+++ src/main/java/com/danielhenry/myblogengine/exception/ArticleNotFoundException.java
@@ -2,7 +2,9 @@ package com.danielhenry.myblogengine.exception;
 
 public class ArticleNotFoundException extends RuntimeException {
 
-	public ArticleNotFoundException(Long id) {
+	private static final long serialVersionUID = 1L;
+
+    public ArticleNotFoundException(Long id) {
 		super("Could not find employee " + id);
 	}
 }
