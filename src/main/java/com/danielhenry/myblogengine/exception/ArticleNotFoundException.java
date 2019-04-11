7e1ea7cd565ab16e836827f9cb96d98a8cf19542
DanielHenry <daniel.henry.39@gmail.com>
2019-04-11 12:35:43 +0700

    Make ArticleNotFoundException constructor public

--- src/main/java/com/danielhenry/myblogengine/exception/ArticleNotFoundException.java
+++ src/main/java/com/danielhenry/myblogengine/exception/ArticleNotFoundException.java
@@ -2,7 +2,7 @@ package com.danielhenry.myblogengine.exception;
 
 public class ArticleNotFoundException extends RuntimeException {
 
-	ArticleNotFoundException(Long id) {
+	public ArticleNotFoundException(Long id) {
 		super("Could not find employee " + id);
 	}
 }
